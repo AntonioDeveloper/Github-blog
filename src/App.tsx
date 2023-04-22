e643d9f28471f551d3572b1528c8c2cbe8b6297d
Antonio <antonio.junior@original.io>
2023-04-22 18:06:46 -0300

    Fixed bug of routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,39 @@ import { BlogContextProvider } from './context/BlogContext';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './components/Router/Router';
 import { GlobalStyle } from './styles/global';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import { DefaultLayout } from './DefaultLayouts';
+import { Home } from './pages/Home/Home';
+import { IssuePage } from './pages/IssuePage/IssuePage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />
+  },
+  {
+    path: "/home",
+    element: <Home />
+  },
+  {
+    path: "issue",
+    element: <IssuePage />
+  }
+]);
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <BlogContextProvider>
-          <Router />
-        </BlogContextProvider>
-        <GlobalStyle />
-      </BrowserRouter>
+      <BlogContextProvider>
+        <RouterProvider router={router} />
+      </BlogContextProvider>
+      <GlobalStyle />
+      {/* <BrowserRouter>
+      </BrowserRouter> */}
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import { DefaultLayout } from './DefaultLayouts';
+import { Home } from './pages/Home/Home';
+import { IssuePage } from './pages/IssuePage/IssuePage';
+
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
